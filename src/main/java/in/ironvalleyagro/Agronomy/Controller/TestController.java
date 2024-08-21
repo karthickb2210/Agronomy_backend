6490a477ecb42ade0a2226bd866a085c4da0f3fd
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2024-08-21 21:16:19 +0530

    edited mail template

--- src/main/java/in/ironvalleyagro/Agronomy/Controller/TestController.java
+++ src/main/java/in/ironvalleyagro/Agronomy/Controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-@CrossOrigin(origins = "http://localhost:5173")
+
 @RestController
 public class TestController {
     @Autowired
@@ -15,9 +15,20 @@ public class TestController {
 
     @PostMapping("/sendMail")
     public void SendMail(@RequestBody MailDetails mailDetails){
-        System.out.println(mailDetails.toString());
-        mailSenderService.sendEmail(mailDetails.getEmail(), "Confirmation on booking",
-                " Your booking has been confirmed successfully");
+        String templates =
+                "Dear " + mailDetails.getName() +",\n\n"+
+                "We are delighted to confirm your appointment for a visit to our farm. Below are the details of your scheduled visit:\n\n" +
+                "Date: "+mailDetails.getDate() +"\n" +
+                "Time: "+mailDetails.getTime()+"\n" +
+                "Location: 2ndAvenueStreet, SaptagiriColony, \n WestJaferkhanpet, K. K. Nagar, \n Chennai, Tamil Nadu 600083\n\n" +
+                "If you have any special requests or need to reschedule, feel free to contact us.\n\n" +
+                "We look forward to welcoming you and showing you around our farm!\n\n" +
+                "Best regards,\n" +
+                "Iron Valley Agronomy\n" +
+                "ironvalleyagro.in";
+        System.out.println(templates);
+        mailSenderService.sendEmail(mailDetails.getEmail(), "Appointment Confirmation for Your Farm Visit on "+mailDetails.getDate()+" at "+ mailDetails.getTime() ,templates
+                );
 
     }
 }
